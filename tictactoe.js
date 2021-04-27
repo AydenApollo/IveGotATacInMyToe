2e24a5a2c009f36066ba045f172f1705d683c705
Shane McDonald <mcdonaldshane33@gmail.com>
2021-04-27 14:57:33 -0700

    what I've got so far

---
+++ tictactoe.js
@@ -0,0 +1,65 @@
+function bind_listeners(){
+    var form = document.querySelector("form");
+    form.addEventListener("submit", cleanse_the_board)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    bind_listeners();
+});
