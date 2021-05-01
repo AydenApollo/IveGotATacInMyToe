16c724d40cdc20f7f9b4c640705a1693e7cddaba
Shane McDonald <mcdonaldshane33@gmail.com>
2021-05-01 13:04:26 -0700

    more updates

--- tictactoe.js
+++ tictactoe.js
@@ -1,12 +1,15 @@
 const theContainer = document.getElementById("theContainer");
-const cellElements = document.querySelectorAll(".cell");
+const cellElements = Array.from(document.querySelectorAll(".cell"));
 const gameBoard = {}
 const oText = "O";
 const xText = "X"
 let xPlayer = xText
+let gameScore = 0; 
 const winningMessage = document.getElementById("winning-message")
 const restartButton = document.getElementById("restartButton");
 const players = document.getElementById("players");
+const DragonFang = document.getElementById("DragonFang")
+const WolfSong = document.getElementById("WolfSong")
 const winningConditions = [
     [0, 1, 2],
     [3, 4, 5],
@@ -19,33 +22,61 @@ const winningConditions = [
 ];
 
 
+const startGame = () => {
+    let xscore = localStorage.getItem("playerXScore")
+    let oscore = localStorage.getItem("playerOScore")
+    if (xscore == null) {
+        localStorage.setItem("playerXScore", 0)
+    }
+    if (oscore == null) {
+        localStorage.setItem("playerOScore", 0)
+    }
+    DragonFang.innerText = "Dragonfang: " + localStorage.getItem("playerXScore")
+    WolfSong.innerHTML = "Wolfsong: " + localStorage.getItem("playerOScore")
+
+}
+
+startGame();
+
+
 function checkGame() {
+    let currentWinner = null
     for(let i = 0; i<cellElements.length; i++) {
         gameBoard[i] = cellElements[i].innerHTML
     winningConditions.forEach(condition => 
     {
-        console.log("condition", condition);
         if (gameBoard[condition[0]] == xText && gameBoard[condition[1]] == xText && gameBoard[condition[2]] == xText) {
-            winningMessage.innerHTML = "Dragon's Blood Is Victorious"
+            winningMessage.innerHTML = "Dragonfang Is Victorious"
+            var dragon = document.querySelector("#dragon");
+            dragon.play();
+            currentWinner = "X"
         }
         if (gameBoard[condition[0]] == oText && gameBoard[condition[1]] == oText && gameBoard[condition[2]] == oText) {
-            winningMessage.innerHTML = "Wolf's Spine Is Victorious"
-        }
+            winningMessage.innerHTML = "Wolfsong Is Victorious"
+            var wolf = document.querySelector("#wolf");
+            wolf.play();
+            img = document.querySelector("#wolfy");
+            img.style.display = 'block';
+            currentWinner = "O"
+        } 
+        
     })
+    if (currentWinner == "X") {
+        localStorage.setItem("playerXScore", parseInt(localStorage.getItem("playerXScore")) + 1)
+    } else if (currentWinner == "O") {
+        localStorage.setItem("playerOScore", parseInt(localStorage.getItem("playerOScore")) + 1)
+    }
 }}
 
 
-
-
-
 cellElements.forEach(cell => {
-    cell.addEventListener("click", forClicked, {once : true})
+     cell.addEventListener("click", forClicked, {once : true})
 })
 
 
 function forClicked (e) {
     const cell = e.target
-    const currentPlayer = xPlayer ? xText : oText 
+    const currentPlayer = xPlayer ? xText : oText
     placeMark(cell, currentPlayer);
     changeSides();
     checkGame();
@@ -60,26 +91,17 @@ function changeSides() {
 
 
 
-
-
-
-
-
-
-function bind_listeners(){
-    var resetBoard = document.querySelector("#restartButton")
-    resetBoard.addEventListener("click", restartButton)
+const resetBoard = () => {
+    cellElements.forEach((cell) => {
+        cell = null
+    })
 }
 
 
+restartButton.addEventListener("click", resetBoard);
+resetBoard();
 
 
 
 
 
-
-
-
-document.addEventListener("DOMContentLoaded", (_event) => {
-    bind_listeners();
-});
